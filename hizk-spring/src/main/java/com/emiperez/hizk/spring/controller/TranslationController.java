221b5d43b3530678e5981c38a1f5e7983da00854
Emilio Perez <emiperez@fiestas.net>
2020-02-26 16:43:32 +0100

    hateaos

--- hizk-spring/src/main/java/com/emiperez/hizk/spring/controller/TranslationController.java
+++ hizk-spring/src/main/java/com/emiperez/hizk/spring/controller/TranslationController.java
@@ -1,9 +1,11 @@
 package com.emiperez.hizk.spring.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.util.List;
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,42 +14,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emiperez.hizk.model.Level;
-import com.emiperez.hizk.model.Text;
 import com.emiperez.hizk.model.Translation;
 import com.emiperez.hizk.model.TranslationId;
 import com.emiperez.hizk.service.TranslationService;
 
-@CrossOrigin(origins = "http://10.0.3.32:4200")
 @RestController
+@RequestMapping("translations")
+@CrossOrigin(origins = "http://10.0.3.32:4200")
 public class TranslationController {
 	
 	private List<Level> levels = List.of(Level.values());
 	@Autowired
 	TranslationService translationService;
 	
-	@GetMapping("/levels")
-	List<Level> listAllLevels() {
-		return levels;
+	@GetMapping("/")
+	public RepresentationModel<?> root() {
+		RepresentationModel<?> rootResource = new RepresentationModel<>();
+		rootResource.add(
+				linkTo(methodOn(TranslationController.class).root()).withSelfRel(),
+				linkTo(methodOn(TranslationController.class).listAllLevels()).withRel("levels"));
+		return rootResource;
 	}
 	
-	@GetMapping("/test")
-	Translation test() {
-		Translation t = new Translation(
-								new Text(new Locale("de"), "und"),
-								new Text(new Locale("es"), "y"),
-								Level.A1);
-		return t;
+	@GetMapping("/levels")
+	public List<Level> listAllLevels() {
+		return levels;
 	}
 		
-	@PostMapping("/translation")
+	@PostMapping()
 	Translation newTranslation(@RequestBody Translation translation) {
 		return translationService.save(translation);
 	}
 	
-	@DeleteMapping("/translation/{originId}/{translationId}")
+	@DeleteMapping("/{originId}/{translationId}")
 	ResponseEntity<Void> deleteTranslation(@PathVariable Integer originId, @PathVariable Integer translationId) {
 		translationService.delete(new TranslationId(originId, translationId));
 		return new ResponseEntity<>(HttpStatus.OK);

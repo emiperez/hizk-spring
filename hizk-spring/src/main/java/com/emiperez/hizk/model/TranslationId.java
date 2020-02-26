93bb97b4c88b80b348690aa9e62699eb0588ff6b
Emilio Perez <emiperez@fiestas.net>
2020-02-26 22:16:06 +0100

    hashcode & equals in TranslationId

--- hizk-spring/src/main/java/com/emiperez/hizk/model/TranslationId.java
+++ hizk-spring/src/main/java/com/emiperez/hizk/model/TranslationId.java
@@ -42,6 +42,37 @@ public class TranslationId implements Serializable{
 		return new TranslationId(translation, origin);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((origin == null) ? 0 : origin.hashCode());
+		result = prime * result + ((translation == null) ? 0 : translation.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TranslationId other = (TranslationId) obj;
+		if (origin == null) {
+			if (other.origin != null)
+				return false;
+		} else if (!origin.equals(other.origin))
+			return false;
+		if (translation == null) {
+			if (other.translation != null)
+				return false;
+		} else if (!translation.equals(other.translation))
+			return false;
+		return true;
+	}
+
 
 	
 }
